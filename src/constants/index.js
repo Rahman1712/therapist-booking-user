c4d31aec43848438db1cea79d39da4e88a7f7e76
abdul rahman <abdulrahman1712@gmail.com>
2023-12-14 17:55:57 +0530

    change urls

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 
-export const API_BASE_URL = "http://localhost:8080/users";
+export const API_BASE_URL = "http://user.medicarehealth.website:8080/users";
 
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = "http://localhost:5173/oauth2/redirect";
+export const OAUTH2_REDIRECT_URI = "https://medicare-eta.vercel.app/oauth2/redirect";
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI + "&user_role=";
 
